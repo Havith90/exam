e3a04b1f01fb8e3fca0c2b3b6ec14c2db31c51c0
Javi Segura <javiera.segurav@gmail.com>
2021-12-17 23:24:20 -0300

    adding

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,4 +1,9 @@
 class Building < ApplicationRecord
 
     has_many :apartments
+
+    validates :name, presence: true, uniqueness: {scope: [:city, :address]}
+    validates :city, presence: true
+    validates :address, presence: true
+
 end
